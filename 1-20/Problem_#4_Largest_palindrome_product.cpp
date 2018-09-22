7714f4229480d26d40e7f2a101bcc97f8f1c6966
Myeongseon Yang <43325270+myeon9seon@users.noreply.github.com>
2018-09-22 14:41:08 +0900

    Update Problem_#4_Largest_palindrome_product.cpp

--- 1-20/Problem_#4_Largest_palindrome_product.cpp
+++ 1-20/Problem_#4_Largest_palindrome_product.cpp
@@ -10,9 +10,8 @@ bool IsPalindrome(int num) {
 		index++;
 	}
 	for (int i = 0; i < index/2; i++) {
-		if (arr[i] != arr[(index-1) - i]) {
+		if (arr[i] != arr[(index-1) - i])
 			return false;
-		}
 	}
 	return true;
 }
