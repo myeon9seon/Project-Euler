935305257d57a3a729ec7371afc712b1c0ede669
Myeongseon Yang <43325270+myeon9seon@users.noreply.github.com>
2018-09-27 11:04:09 +0900

    Create Problem_#9_Special_Pythagorean_triplet.cpp
    https://projecteuler.net/problem=9

---
+++ 1-20/Problem_#9_Special_Pythagorean_triplet.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+bool IsNatural(double dNum) {
+	int nNum = dNum;
+	if (dNum - nNum <= 0) return true;
+	return false;
+}
+
+int main(void) {
+	const int MAX = 1000;
+	for (int a = 1; a < MAX; a++) {
+		for (int b = a; b < MAX; b++) {
+			double c = sqrt(pow(a, 2.0) + pow(b, 2.0));
+			if (IsNatural(c)) {
+				if (a + b + c == 1000) {
+					cout << a << "＋" << b << "＋" << c << "＝" << a + b + c << endl;
+					cout << a << "×" << b << "×" << c << "＝" << (int)(a * b * c) << endl;
+				}
+			}
+		}
+	}
+}
