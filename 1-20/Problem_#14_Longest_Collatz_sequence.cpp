c80cd5ec16aca965daac54883e03a993d89d8474
Myeongseon Yang <43325270+myeon9seon@users.noreply.github.com>
2018-10-02 12:11:52 +0900

    Update Problem_#14_Longest_Collatz_sequence.cpp

--- 1-20/Problem_#14_Longest_Collatz_sequence.cpp
+++ 1-20/Problem_#14_Longest_Collatz_sequence.cpp
@@ -1,4 +1,4 @@
-// Solution 1
+// Solution 1-1: sequence function
 // execution time: 11241ms
 
 #include <iostream>
@@ -21,6 +21,35 @@ int main(void) {
 		int count = 0;
 		while (collatz != 1) {
 			collatz = collatzSequence(collatz);
+			count++;
+		}
+		if (count > maxCount) {
+			maxCount = count;
+			maxNum = num;
+		}
+	}
+	cout << maxNum << endl;
+
+	end = clock();
+	cout << "execution time: " << end - begin << "ms" << endl;
+}
+
+// Solution 1-2: function to line
+// execution time: 5827ms
+
+#include <iostream>
+#include <ctime>
+using namespace std;
+
+int main(void) {
+	clock_t begin = clock(), end;
+	int maxCount = 0, maxNum = 0;
+
+	for (int num = 1; num <= 1000000; num++) {
+		__int64 collatz = num;
+		int count = 0;
+		while (collatz != 1) {
+			(collatz % 2 == 0) ? collatz /= 2 : collatz = (collatz * 3) + 1;
 			count++;
 		}
 		if (count > maxCount) {
