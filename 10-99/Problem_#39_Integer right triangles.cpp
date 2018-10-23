031f90744f9f38fe2cb3b80ca84199212354e17a
Myeongseon Yang <43325270+myeon9seon@users.noreply.github.com>
2018-10-23 19:53:06 +0900

    Create Problem_#39_Integer right triangles.cpp
    https://projecteuler.net/problem=39

---
+++ 10-99/Problem_#39_Integer right triangles.cpp
@@ -0,0 +1,41 @@
+// execution time: 3564ms
+
+#include <iostream>
+#include "Time.h"
+using namespace std;
+
+const int MAX = 1000;
+
+bool isRightAngled(int a, int b, int c) {
+	if (a * a + b * b == c * c)
+		return true;
+	else
+		return false;
+}
+
+int main() {
+	Time t;
+	int triLen[MAX + 1] = { 0, };
+	int maxCnt = 0, maxLen = 0;
+	for (int a = 1; a <= MAX; a++) {
+		for (int b = 1; b <= MAX; b++) {
+			if (b > a) { // 두 번 이상 계산하지 않기 위함
+				for (int c = 1; c <= MAX; c++) {
+					int len = a + b + c;
+					if (len > MAX) break;
+					if (isRightAngled(a, b, c))
+						triLen[len]++;
+						//cout << a << ',' << b << ',' << c << ": " << len << endl;
+				}
+			}
+		}
+	}
+	for (int i = 0; i < MAX; i++) {
+		if (triLen[i] > maxCnt) {
+			maxCnt = triLen[i];
+			maxLen = i;
+		}
+	}
+	cout << maxLen << endl;
+	t.printRunTime();
+}
