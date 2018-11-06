59e36f8714c5b2ef26d48aa2272d2e6002f3bf23
Myeongseon Yang <43325270+myeon9seon@users.noreply.github.com>
2018-11-06 15:55:32 +0900

    Update Problem_#53_Combinatoric selections.cpp
    https://projecteuler.net/problem=53

--- 10-99/Problem_#53_Combinatoric selections.cpp
+++ 10-99/Problem_#53_Combinatoric selections.cpp
@@ -1,44 +1,61 @@
-// 미완
+// execution time: 56ms
 
 #include <iostream>
+#include <cmath>
 #include "Time.h"
 using namespace std;
 
-int getCombination(int n, int r) {
-	int *numerator = new int[r];
-	int *denominator = new int[r];
-	int num = 1, denom = 1;
-	
-	for (int i = 0; i < r; i++) {
-		numerator[i] = n - i;
-		denominator[i] = r - i;
+void setPrimeFactorization(int *divCount, int num) {
+	for (int div = 2; div <= num; div++) {
+		if (num % div == 0) {
+			divCount[div]++;
+			num /= div;
+			div = 1;
+		}
 	}
-	for (int i = r - 1; i >= 0; i--) {
-		for (int k = r - 1; k >= 0; k--) {
-			if (numerator[i] % denominator[i] == 0) {
-				numerator[i] /= denominator[i];
-				denominator[i] = 1;
+}
+
+bool isOverCombination(int n, int r) {
+	const int OVER = 1000000;
+	int *divNum = new int[n + 1];
+	int *divDenom = new int[n + 1];
+	int combination = 1;
+
+	for (int i = 0; i <= n; i++) {
+		divNum[i] = 0;
+		divDenom[i] = 0;
+	}
+	for (int i = 0; i < r; i++) {
+		setPrimeFactorization(divNum, n - i);
+		setPrimeFactorization(divDenom, r - i);
+	}
+	for (int i = 2; i <= n; i++)
+		divNum[i] -= divDenom[i];
+	for (int i = n; i > 1; i--) {
+		if (divNum[i] > 0) {
+			combination *= (int)pow(i, divNum[i]);
+			if (combination > OVER) {
+				delete[] divNum, divDenom;
+				return true;
 			}
 		}
 	}
-	for (int i = 0; i < r; i++) {
-		num *= numerator[i];
-		denom *= denominator[i];
-	}
-	delete[] numerator, denominator;
-	return num / denom;
+	delete[] divNum, divDenom;
+	return false;
+}
+
+int getOverCombiCount() {
+	const int MAX_N = 100;
+	int count = 0;
+	for (int n = 1; n <= MAX_N; n++)
+		for (int r = 1; r <= n; r++)
+			if (isOverCombination(n, r))
+				count++;
+	return count;
 }
 
 int main() {
 	Time t;
-	int count = 0;
-	for (int n = 1; n <= 100; n++) {
-		for (int r = 1; r <= n / 2; r++) {
-			int combination = getCombination(n, r);
-			if (combination >= 1000000)
-				count++;
-		}
-	}
-	cout << count << endl;
+	cout << getOverCombiCount() << endl;
 	t.printRunTime();
 }
